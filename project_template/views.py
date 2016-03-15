fba70b276a4e41f5f08863f8be4d48485f3e124a
Qian Wang <qianwang@us.ibm.com>
2016-03-14 21:25:33 -0400

    instruction

--- project_template/views.py
+++ project_template/views.py
@@ -14,31 +14,15 @@ def index(request):
     if request.GET.get('search'):
         search = request.GET.get('search')
         output_list = find_similar(search)
-        paginator = Paginator(output_list, 10) # Shows only 10 records per page
+        paginator = Paginator(output_list, 10)
         page = request.GET.get('page')
         try:
             output = paginator.page(page)
         except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
             output = paginator.page(1)
         except EmptyPage:
-        # If page is out of range (e.g. 7777), deliver last page of results.
             output = paginator.page(paginator.num_pages)
     return render_to_response('project_template/index.html', 
                           {'output': output,
                            'magic_url': request.get_full_path(),
                            })
-
-    #=======================================================================================
-    #OLD IMPLEMENTATION
-    # if request.method == 'POST':
-    #  	form = QueryForm(request.POST)
-    #  	if form.is_valid():
-    #  		q = form.cleaned_data['q']
-    #  		output = find_similar(q)
-    #  		return render(request, 'project_template/index.html', {'output': output[:10]})
-    #  		#return HttpResponse('Similar Messages (top 10): \n'+output)
-    # else:
-    #     form = QueryForm()
-    #     return render(request, 'project_template/index.html', {'form': form})
-    #=======================================================================================
