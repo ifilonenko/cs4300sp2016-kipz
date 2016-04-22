b1bae12b94f5da7c158c20e5471e1e345c55e16b
Kelsey Duncan <ked83@cornell.edu>
2016-04-21 22:57:12 -0400

    functionality for two search bars

--- project_template/views.py
+++ project_template/views.py
@@ -9,13 +9,40 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def index(request):
+	beer_output_list = ''
+	features_output_list = ''
     output_list = ''
     output=''
-    if request.GET.get('search'):
+    if request.GET.get('search') and request.GET.get('search_features'):
+    	beer_search = request.GET.get('search')
+    	features_search = request.GET.get('search_features')
+    	beer_output_list = find_similar(beer_search)
+    	#features_output_list = find_similar_features(features_search)
+    	#paginator_feat = Paginator(features_output_list, 10)
+    	paginator_beer = Paginator(beer_output_list, 10)
+    	page = request.GET.get('page')
+    	try:
+            output = paginator_beer.page(page)
+        except PageNotAnInteger:
+            output = paginator_beer.page(1)
+        except EmptyPage:
+            output = paginator_beer.page(paginator_beer.num_pages)
+    else if request.GET.get('search'):
         search = request.GET.get('search')
         output_list = find_similar(search)
         paginator = Paginator(output_list, 10)
         page = request.GET.get('page')
+        try:
+            output = paginator.page(page)
+        except PageNotAnInteger:
+            output = paginator.page(1)
+        except EmptyPage:
+            output = paginator.page(paginator.num_pages)
+    else if request.GET.get('search_features'):
+    	features_search = request.GET.get('search_features')
+    	features_output_list = find_similar_features(features_search)
+    	paginator = Paginator(features_output_list, 10)
+    	page = request.GET.get('page')
         try:
             output = paginator.page(page)
         except PageNotAnInteger:
