bd38e82d23f3b5ed2168c0b07b297b4a0631ead9
Ilan Filonenko <ifilondz@gmail.com>
2016-04-22 16:16:53 -0400

    implemented rochhio

--- project_template/test.py
+++ project_template/test.py
@@ -40,39 +40,29 @@ file5 = urllib2.urlopen('https://s3.amazonaws.com/stantemptesting/index_to_vocab
 index_to_vocab = json.load(file5, object_hook=json_numpy_obj_hook)
 file6 = urllib2.urlopen('https://s3.amazonaws.com/stantemptesting/vocab_to_index.json')
 vocab_to_index = json.load(file6, object_hook=json_numpy_obj_hook)
-def get_sim(beer1, beer2):
-    """
-    Arguments:
-        beer1: The name of the first beer we are looking for.
-        beer2: The name of the second beer we are looking for.
-    
-    Returns:
-        similarity: Cosine similarity of the two beer review(s).
-    """
-    doc_of_beer_1 = doc_voc_matrix[beer_name_to_index[beer1]]
-    doc_of_beer_2 = doc_voc_matrix[beer_name_to_index[beer2]]   
-    numerator = np.dot(doc_of_beer_1,doc_of_beer_2)
-    return numerator/(LA.norm(doc_of_beer_1)* LA.norm(doc_of_beer_2))
 
-def top_similar(beer, k=10):
-    """
-        Arguments:
-        beer: The name of the beer we are looking for.
-        k: Number of top terms to return
-    
-    Returns:
-        result: List of the top k similar beers 
 
-    """
-    beer_index = beer_name_to_index[beer]
-    filtered_s =  beer_sims[beer_index].argsort()[-(k+1):][::-1]
+def closest_beers(beers_set, beer_index_in, k = 5):
+    beers_compressed = normalize(beers_set, axis = 1)
+    beers_vector = beers_compressed[beer_index_in,:]
+    sims = (np.dot(beers_compressed,beers_vector))/(LA.norm(beers_compressed)* LA.norm(beers_vector))
+    asort = np.argsort(-sims)[:k+1]
     result = []
-    for elem in filtered_s:
-        if elem != beer_index:
-            result.append((beer_index_to_name[elem],get_sim(beer, beer_index_to_name[elem])))
+    for i in asort[1:]:
+        result.append(beer_index_to_name[i])
     return result
 
-def rocchio(query, relevant, irrelevant, a=.3, b=.3, c=.8, clip = True):
+def closest_features(features_set, feature_index_in, k = 5):
+    features_compressed = normalize(features_set.T, axis = 1)
+    feature_vector = features_compressed[feature_index_in,:]
+    sims = (np.dot(features_compressed,feature_vector))/(LA.norm(features_compressed)* LA.norm(feature_vector))
+    asort = np.argsort(-sims)[:k+1]
+    result = []
+    for i in asort[1:]:
+        result.append((index_to_vocab[str(i)],sims[i]/sims[asort[0]]))
+    return result
+    
+def rocchio(q, relevant, irrelevant, a=.3, b=.3, c=.8, clip = True):
     '''
     Arguments:
         query: a string representing the name of the beer being queried for
@@ -90,13 +80,13 @@ def rocchio(query, relevant, irrelevant, a=.3, b=.3, c=.8, clip = True):
         q_mod: a vector representing the modified query vector. this vector should have no negatve
         weights in it!
     '''
-    query_index = beer_name_to_index[query]
-    relevant_vectors = [doc_voc_matrix[beer_name_to_index[x]] for x in relevant] 
+    beers_vector = beers_compressed[beer_name_to_index[q],:]
+    relevant_vectors = [beers_compressed[beer_name_to_index[x],:] for x in relevant] 
     relevant_vector = sum(x for x in relevant_vectors)
-    irrelevant_vectors =[doc_voc_matrix[_name_to_index[x]] for x in irrelevant]  
+    irrelevant_vectors =[beers_compressed[beer_name_to_index[x],:] for x in irrelevant]  
     irrelevant_vector = sum(x for x in irrelevant_vectors)
     
-    first_term = (a*doc_voc_matrix[query_index])
+    first_term = (a*beers_vector)
     if not relevant:
         second_term = 0
     else:
@@ -123,26 +113,10 @@ def roccio_with_pseudo(q, k = 10):
         two beer review(s).
     """
     '''
-    mean_average_percision = []
-    query_index = beer_name_to_index[q]
-    query_initial_vector = doc_voc_matrix[query_index]
-    cos_sim_init = np.array([ np.dot(query_initial_vector,vector) for vector in doc_voc_matrix ]).argsort()[::-1]
-    cos_indexes_init = []
-    for elem in cos_sim_init:
-        if elem != beer_name_to_index[q]:
-            cos_indexes_init.append(elem)
-    pseudo_relevant = [beer_index_to_name[x] for x in cos_indexes_init][:k]
+    pseudo_relevant = closest_beers(beers_compressed,beer_name_to_index[q],10)
     query_vector = rocchio(q,pseudo_relevant,[])
-    cos_sim = np.array([ np.dot(query_vector,vector) for vector in doc_voc_matrix ]).argsort()[::-1]
-    cos_indexes = []
-    for elem in cos_sim:
-        if elem != beer_name_to_index[q]:
-            cos_indexes.append(elem)
-    return ([beer_index_to_name[x] for x in cos_indexes][:k])
-
-def closest_projects(beers_set, project_index_in, k = 5):
-    beers_compressed = normalize(beers_set, axis = 1)
-    sims = beers_compressed.dot(beers_compressed[project_index_in,:])
+    beers_compressed_2 = normalize(beers_compressed, axis = 1)
+    sims = (np.dot(beers_compressed_2,query_vector))/(LA.norm(beers_compressed_2)* LA.norm(query_vector))
     asort = np.argsort(-sims)[:k+1]
     result = []
     for i in asort[1:]:
@@ -150,18 +124,9 @@ def closest_projects(beers_set, project_index_in, k = 5):
     return result
 
 def find_similar(q):
-	print("Searching %d beers" % len(beer_index_to_name))
-	return closest_projects(beers_compressed, beer_name_to_index[q], 50)
-
-def closest_features(features_set, feature_index_in, k = 5):
-    features_compressed = normalize(features_set.T, axis = 1)
-    sims = features_compressed.dot(features_compressed[feature_index_in,:])
-    asort = np.argsort(-sims)[:k+1]
-    result = []
-    for i in asort[1:]:
-        result.append((index_to_vocab[str(i)],sims[i]/sims[asort[0]]))
-    return result
+    print("Searching %d beers" % len(beer_index_to_name))
+    return roccio_with_pseudo(q, 50)
 
 def find_similar_features(q):
-    print("Searching %d features" % len(index_to_vocab))
+    print("Searching %d features" % len(vocab_to_index))
     return closest_features(features_compressed, vocab_to_index[q], 50)
